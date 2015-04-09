30490662926c0ef4133afe8233ce7976c53335e3
sethu1984 <mca.sethuraman@gmail.com>
2015-04-09 17:00:28 +0530

    Required Areas Are added

---
+++ ProjectMaintenance/Areas/Master/MasterAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace ProjectMaintenance.Areas.MasterRecord
+{
+    public class MasterRecordAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "MasterRecord";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "MasterRecord_default",
+                "MasterRecord/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ ProjectMaintenance/Areas/Softwares/SoftwaresAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace ProjectMaintenance.Areas.Softwares
+{
+    public class SoftwaresAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Softwares";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Softwares_default",
+                "Softwares/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
